9d2f9f754611e8129f7a1e90e1ba54fb47401eb5
bcc32 <bcc32@anapnea.net>
2011-12-31 17:20:16 -0500

    solve 1225

--- 1225/sol.cc
+++ 1225/sol.cc
@@ -1,7 +1,7 @@
-// FIXME SLOW
 #include <iostream>
 using namespace std;
 
+/* XXX SLOW
 long long poss(char snd, char fst, int n)
 {
     if (n == 0)
@@ -15,10 +15,33 @@ long long poss(char snd, char fst, int n)
     else
         return poss(' ', 'R', n - 1) + poss(' ', 'W', n - 1);
 }
+*/
+
+// XXX NB
+// Fibonacci because of recursion evident in previous algorithm
+// Revised to use iterative algorithm to avoid TLE
+
+unsigned int poss(int n)
+{
+    int x = 2, y = 2;
+    if (n == 1)
+        return 2;
+    else if (n == 2)
+        return 2;
+    else
+        for (int i = 2; i < n; i++)
+        {
+            int t = y;
+            y = x + y;
+            x = t;
+        }
+    return y;
+}
 
 int main(void)
 {
     int n;
     cin >> n;
-    cout << poss(' ', ' ', n) << endl;
+    // cout << poss(' ', ' ', n) << endl;
+    cout << poss(n) << endl;
 }
